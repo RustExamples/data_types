6265ecf4dda98fbe5c9e4e8fdefb04ef0269f89c
Meenakshi Sundaram <vms20591@gmail.com>
2017-12-11 00:15:02 +0530

    Scalar Types - Boolean

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,7 @@
 fn main() {
-    // addition
-    let sum = 5 + 10;
-    println!("sum is {}", sum);
+    let yes = true;
+    println!("yes is {}", yes);
 
-    // subtraction
-    let difference = 10 - 5;
-    println!("difference is {}", difference);
-
-    // multiplication
-    let product = 10 * 5;
-    println!("product is {}", product);
-
-    // division
-    let quotient = 10/5;
-    println!("quotient is {}", quotient);
-
-    // remainder
-    let remainder = 10%5;
-    println!("remainder is {}", remainder);
+    let no:bool = false;
+    println!("no is {}", no);
 }
